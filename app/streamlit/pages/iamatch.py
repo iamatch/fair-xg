f0a062cd453c2f99f743383152a1ef2fee09be0e
Rémi Verdier <remi@shipfix.com>
2021-07-16 19:01:21 +0200

    Add statsbomb data sources

--- app/streamlit/pages/iamatch.py
+++ app/streamlit/pages/iamatch.py
@@ -2,6 +2,5 @@ import streamlit as st
 
 
 def write():
-    link = '[GitHub](http://github.com/iamatch)'
-    st.markdown(link, unsafe_allow_html=True)
-
+    st.markdown('Follow us on [GitHub](http://github.com/iamatch)', unsafe_allow_html=True)
+    st.markdown('Data sources [StatsBomb](https://github.com/statsbomb/open-data)')
